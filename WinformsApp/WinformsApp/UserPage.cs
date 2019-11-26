6b37726b9c5837b1102ab24702f27a554c55c223
OlegBezr <obezrukavnikov@gmail.com>
2019-11-26 16:17:07 +0300

    Forms for different windows created

--- WinformsApp/WinformsApp/UserPage.cs
+++ WinformsApp/WinformsApp/UserPage.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinformsApp
 {
-    public partial class Form1 : Form
+    public partial class UserPage : Form
     {
-        public Form1()
+        public UserPage()
         {
             InitializeComponent();
         }

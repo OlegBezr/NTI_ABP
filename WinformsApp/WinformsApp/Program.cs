2a9d4c1ca2a275d56c31f0a0f9d5e219ce1d0d10
OlegBezr <obezrukavnikov@gmail.com>
2019-12-05 23:14:07 +0300

    first task completed

--- WinformsApp/WinformsApp/Program.cs
+++ WinformsApp/WinformsApp/Program.cs
@@ -16,7 +16,7 @@ namespace WinformsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AdminPage());
+            Application.Run(new LoginPage());
         }
     }
 }
--- WinformsApp/WinformsApp/UserPage.Designer.cs
+++ WinformsApp/WinformsApp/UserPage.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label3
@@ -61,25 +60,11 @@
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(199)))), ((int)(((byte)(77)))));
-            this.button1.Location = new System.Drawing.Point(680, 380);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 50);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Click here";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // UserPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Name = "UserPage";
@@ -92,6 +77,5 @@
 
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
     }
 }

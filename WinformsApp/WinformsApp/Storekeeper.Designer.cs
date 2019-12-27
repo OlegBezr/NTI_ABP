4be8c48a5939001eed791d5912e11dec94b9422d
OlegBezr <obezrukavnikov@gmail.com>
2019-12-27 19:27:40 +0300

    Work with excel in progress

--- WinformsApp/WinformsApp/Storekeeper.Designer.cs
+++ WinformsApp/WinformsApp/Storekeeper.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +63,32 @@
             this.textBox1.Size = new System.Drawing.Size(427, 32);
             this.textBox1.TabIndex = 2;
             // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(476, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Submit data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(345, 291);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(271, 20);
+            this.textBox3.TabIndex = 4;
+            // 
             // Storekeeper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
@@ -81,5 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
--- WinformsApp/WinformsApp/Storekeeper.cs
+++ WinformsApp/WinformsApp/Storekeeper.cs
@@ -7,18 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using OfficeOpenXml;
+using IronXL;
+using System.Threading;
 
 namespace WinformsApp
 {
     public partial class Storekeeper : Form
     {
+        private string[] columnNames = new string[5]{ "NAME", "TYPE", "WEIGHT", "ARTICLE", "YEAROFISSUE" };
+        private char[] columns = new char[5] { 'A', 'B', 'C', 'D', 'E' };
+        private string[] rowData = new string[5] { "", "", "", "", "" };
+
         public Storekeeper()
         {
             InitializeComponent();
         }
 
         OpenFileDialog dialog = new OpenFileDialog();
-
         private void button1_Click(object sender, EventArgs e)
         {
             dialog.Filter = "XLSX, XLS, CSV|*.xlsx;*.xls;*.csv";
@@ -26,6 +33,44 @@ namespace WinformsApp
             {
                 textBox1.Text = dialog.FileName;
                 textBox2.Text = dialog.SafeFileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //call function with the path to the file
+            readXLS(textBox1.Text);
+        }
+
+        public void readXLS(string FilePath)
+        {
+            WorkBook workbook = WorkBook.Load(FilePath);
+            WorkSheet sheet = workbook.WorkSheets.First();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (sheet[columns[i] + "1"].StringValue != columnNames[i])
+                {
+                    textBox3.Text = "Неверный формат файла!";
+                    return;
+                }
+            }
+
+            //run throw rows
+            for (int i = 1; i < 1000000; i++)
+            {
+                if (sheet['A' + i.ToString()].StringValue == "")
+                {
+                    break;
+                }
+
+                //run throw columns
+                for (int j = 0; j < 5; j++)
+                {
+                    rowData[j] = sheet[columns[j] + i.ToString()].StringValue;
+                }
+
+                textBox3.Text = rowData[3];
             }
         }
     }
